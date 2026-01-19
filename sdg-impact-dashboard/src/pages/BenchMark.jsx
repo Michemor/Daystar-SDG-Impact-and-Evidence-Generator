e6599255a156a76db1a4552d21863e466567f3e4
Michemor <yatoijema@gmail.com>
2026-01-19 22:21:41 +0300

    Benchmark page

--- sdg-impact-dashboard/src/pages/BenchMark.jsx
+++ sdg-impact-dashboard/src/pages/BenchMark.jsx
@@ -1,6 +1,14 @@
 import { Paper, Typography } from "@mui/material"
 
 export default function Benchmark() {
+
+    const institutionalData =[
+        { university: "Daystar University", impactScore: 85, sdgCoverage: 12 },
+        { university: "MelWater University", impactScore: 78, sdgCoverage: 10 },
+        { university: "ValleyBridge University", impactScore: 90, sdgCoverage: 14 },
+        { university: "ModernWake University", impactScore: 65, sdgCoverage: 8 },
+        { university: "GreenField University", impactScore: 82, sdgCoverage: 11 },
+    ]
     return (
         <>
         <Paper
